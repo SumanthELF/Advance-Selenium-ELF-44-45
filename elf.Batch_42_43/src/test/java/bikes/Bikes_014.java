37a5f5df9ce72cd3b656d3233143a023b7e4e8ba
Sumanth <Sumanth@TYSS-Sumanth>
2024-04-03 17:13:29 +0530

    Adding 3 new files

---
+++ elf.Batch_42_43/src/test/java/bikes/Bikes_014.java
@@ -0,0 +1,10 @@
+package bikes;
+
+public class Bikes_014 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ elf.Batch_42_43/src/test/java/demo/copy/OpenSwiggyTest.java
@@ -0,0 +1,22 @@
+package demo.copy;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class OpenSwiggyTest {
+
+	@Test
+	public void swiggy() {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+		
+		driver.get("https://www.swiggy.com/");
+	}
+}
